1f6f4a20fdd19d97fb50ecd953709f0626f8d2e9
devindubin <68442791+devindubin@users.noreply.github.com>
2023-02-23 22:13:21 +0000

    created openai function for default image

--- backend/app/services/items.py
+++ backend/app/services/items.py
@@ -1,5 +1,6 @@
 from slugify import slugify
-
+import requests
+import os
 from app.db.errors import EntityDoesNotExist
 from app.db.repositories.items import ItemsRepository
 from app.models.domain.items import Item
@@ -21,3 +22,19 @@ def get_slug_for_item(title: str) -> str:
 
 def check_user_can_modify_item(item: Item, user: User) -> bool:
     return item.seller.username == user.username
+
+def get_dalle_default_image(title: str) -> dict:
+    url = "https://api.openai.com/v1/images/generations"
+    headers = {'Authorization':'Bearer '+ os.getenv('OPEN_API_KEY')}
+    params = {
+        'prompt': title
+    }
+    with requests.Session() as s:
+        response = s.post(url,headers=headers,params=params)
+        if response.ok:
+            gen_image = response.json()
+        else:
+            raise Exception(response.text)
+    
+    image_url = gen_image['data'][0]['url']
+    return image_url
